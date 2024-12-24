a3eb9866e42e70b08779f9497df472dcd518cfb7
Iván Manzaba <mauricio.9.inm@gmail.com>
2024-12-24 17:19:29 -0500

    Add support for rate control and cache management in the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { CacheModule } from '@nestjs/cache-manager';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 15,
+      },
+    ]),
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 2000,
+      max: 1000,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, Logger],
+  providers: [
+    AppService,
+    Logger,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,6 @@ export class AppService {
   constructor(private logger: Logger) {}
 
   getHello(): string {
-    this.logger.error('Hello World!', AppService.name);
     return 'Hello World!';
   }
 }
